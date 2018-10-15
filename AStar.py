172dd22135efc64cc8ccf679ed10991c43032a41
qcuong98 <tranquoccuong@outlook.com.vn>
2018-10-15 19:44:51 +0700

    add a-star algo

--- AStar.py
+++ AStar.py
@@ -7,6 +7,7 @@ n = int(f.readline())
 Sx, Sy = [int(x) for x in next(f).split()]
 Gx, Gy = [int(x) for x in next(f).split()]
 arr = [[int(x) for x in line.split()] for line in f]
+visited = set()
 
 def sqr(a):
     return a * a
@@ -15,11 +16,56 @@ def sqr(a):
 def heuristic(x, y):
     return math.sqrt(sqr(Gx - x) + sqr(Gy - y))
 
-def aStar(Sx, Sy, Gx, Gy, arr):
-    return -1
+# cells that robot can go from (x, y)
+def listCellsCanGoFrom(x, y):
+    ans = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            (u, v) = (x + i, y + j)
+            if (u in range(n)) and (v in range(n)) and \
+               (arr[u][v] != 1) and ((u, v) not in visited):
+                ans.append((u, v))
+    return ans
+                
+def aStar(Sx, Sy, Gx, Gy):
+    g = [x[:] for x in [[float("inf")] * n] * n]  
+    h = [x[:] for x in [[0] * n] * n]  
+    f = [x[:] for x in [[0] * n] * n] 
+    tr = [x[:] for x in [[(0, 0)] * n] * n]   
 
-print(n)
-print(Sx, Sy)
-print(Gx, Gy)
-print(arr)
-print(heuristic(3, 5))
+    g[Sx][Sy] = 0
+    for i in range(n):
+        for j in range(n):
+            h[i][j] = heuristic(i, j)
+            f[i][j] = g[i][j] + h[i][j]
+
+    heap = [(f[Sx][Sy], Sx, Sy)]
+    heapq.heapify(heap)
+
+    while (len(heap) != 0):
+        (ff, x, y) = heapq.heappop(heap)
+
+        if (x, y) == (Gx, Gy):
+            path = []
+            while (x, y) != (Sx, Sy):
+                path.append((x, y))
+                (x, y) = tr[x][y]
+            path.append((Sx, Sy))
+            path.reverse()
+            return (ff, path)
+
+        if (x, y) not in visited:
+            visited.add((x, y))
+
+            listNextCells = listCellsCanGoFrom(x, y)
+            for (u, v) in listNextCells:
+                cost = g[x][y] + 1 
+                if (cost < g[u][v]):
+                    tr[u][v] = (x, y)
+                    g[u][v] = cost
+                    f[u][v] = cost + h[u][v]
+                    heapq.heappush(heap, (f[u][v], u, v))
+    
+    return (-1, [])
+
+print(aStar(Sx, Sy, Gx, Gy))
